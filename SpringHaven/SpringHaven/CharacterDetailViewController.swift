c78ff620dd7724e69bbc11cf789afeafa5b75daa
SebasP629 <55610650+SebasP629@users.noreply.github.com>
2023-11-05 21:50:23 -0500

    Add files via upload

---
+++ SpringHaven/SpringHaven/CharacterDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterDetailViewController.swift
+//  SpringHaven
+//
+//  Created by Sebastian Pena on 11/5/23.
+//
+
+import UIKit
+
+class CharacterDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
